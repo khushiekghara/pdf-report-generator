bd2391a926c15628bbacc7228a1f19fd6b4b024e
khushiekghara <khushiekghara92@gmail.com>
2026-08-26 22:23:19 +0530

    Stage 0: setup ready

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
